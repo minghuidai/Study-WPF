5267a1f99e1aad3331fdc7f960aaccb59522c76f
minghuidai <matthew.dai@tokiomarinetech.com>
2015-03-04 17:32:52 -0500

    added unit test for bing maps

---
+++ Study.WPF.UnitTests/BingMapQueries/Test_BingMapFunctions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Maps.MapControl.WPF;
+using Study.BingMap.Query;
+using System.Linq;
+
+namespace Study.WPF.UnitTests.BingMapQueries
+{
+    [TestClass]
+    public class Test_BingMapFunctions
+    {
+
+        string _Key = "Ai6zQ5AwxFAZKY3DtRmKAPJHZVlK4h_e01jNbblWGbagsXzwH0nf5vYrTEr13kBd";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            // add locations into the polygon
+            var locCollection = new LocationCollection();
+            locCollection.Add(new Location(47.6424, -122.3219));
+            locCollection.Add(new Location(47.8424, -122.1747));
+            locCollection.Add(new Location(47.5814, -122.1747));
+
+            // define a polycy
+            var p = new MapPolygon() { 
+                Locations = locCollection,
+            };
+
+            var filterString = Helper.GetSpatialFilterString(p);
+            Assert.AreEqual("spatialFilter=intersects('POLYGON ((47.6424 -122.3219,47.8424 -122.1747,47.5814 -122.1747))')", filterString);
+        }
+
+
+        [TestMethod]
+        public void Test_A_Sample()
+        {
+
+            var mapQuery = new BingMapQuery(_Key);
+            var result = mapQuery.SampleQuery();
+
+            Assert.IsTrue(result.Count() > 0);
+
+        }
+
+    }
+}
---
+++ Study.WPF.UnitTests/BingMapQueries/Test_SpatialQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Study.BingMap.Query;
+
+namespace Study.WPF.UnitTests.BingMapQueries
+{
+    [TestClass]
+    public class Test_SpatialQuery
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var ddd = new SpatialService("Ai6zQ5AwxFAZKY3DtRmKAPJHZVlK4h_e01jNbblWGbagsXzwH0nf5vYrTEr13kBd");
+
+            //var result1 = ddd.ExampleFindByAreaRadius();
+            //var result2 = ddd.ExampleFindByProperty();
+            //var result = ddd.ExampleFindByBoundingBox();
+            //var result4 = ddd.ExampleQueryByIdAtom();
+
+            var result = ddd.ReverseGeoCode(33.75550, -84.3900);
+            Assert.IsTrue(result.Count > 0);
+            //Assert.IsTrue(result2.Count > 0);
+            //Assert.IsTrue(result3.Count > 0);
+            //Assert.IsTrue(result4 != null);
+            
+        }
+    }
+}
